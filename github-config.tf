0a0d4157d753e254fdff64d6825f9101f3127fe8
Daniel Hill <dan@mamu.co>
2020-10-20 10:52:18 +0100

    remove branch protection (only supported in orgs)

--- github-config.tf
+++ github-config.tf
@@ -13,21 +13,6 @@ resource "github_repository" "github_config" {
   }
 }
 
-resource "github_branch_protection" "github_config_main" {
-  branch         = github_repository.github_config.default_branch
-  repository     = github_repository.github_config.name
-  enforce_admins = false
-
-  required_status_checks {
-    strict = true
-  }
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews      = true
-    require_code_owner_reviews = true
-  }
-}
-
 resource "github_issue_label" "github_config" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
